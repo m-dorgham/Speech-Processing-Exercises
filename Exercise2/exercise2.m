0f66ac6685678aaf478e35af5039b82ec4ef9a45
Dorgham <dorgham@attenio.de>
2017-12-07 21:00:14 +0100

    complete exercise 2

--- Exercise2/exercise2.m
+++ Exercise2/exercise2.m
@@ -45,6 +45,22 @@ function v_signal = my_inverse_stft_2(m_stft, fs, frame_length, frame_shift, v_s
 	endfor
 endfunction
 
+function [m_frames, v_time_frame] = my_windowing(v_signal, sampling_rate, frame_length, frame_shift)
+	m_frames_width = frame_length*sampling_rate;
+	m_frames_height= length(v_signal)/(frame_shift*sampling_rate);
+	i = 1;
+	for j = 1:frame_shift*sampling_rate:length(v_signal)
+		v_time_frame(i)=j+m_frames_width-(m_frames_width/2)-1;
+		if(j+m_frames_width-1 < length(v_signal))
+			m_frames(i, 1:m_frames_width) = v_signal(j:j+m_frames_width-1);
+		else
+			remaining_elems = length(v_signal)-j;
+			m_frames(i, 1:remaining_elems) = v_signal(j:j+remaining_elems-1);
+			m_frames(i, remaining_elems+1:m_frames_width) = zeros(1, m_frames_width-remaining_elems);
+		endif
+		i++;
+	endfor
+endfunction
 
 %subsection 1
 frame_length=0.032;
@@ -53,9 +69,30 @@ frame_shift=0.008;
 [m_stft_2, v_freq_2, v_time_2] = my_stft(M_2, fs_2, frame_length, frame_shift, hann(frame_length*fs_2, 'periodic'));
 
 %subsection 2
+[m_frames_1, v_time_frame_1] = my_windowing(M_1, fs_1, 0.032, 0.016);
+for i=1:length(m_frames_1(:,1))
+	flipped_frame =  fliplr(m_frames_1(i,:));
+	acf_1(i,1:length(m_frames_1(i,:))*2-1) = conv(m_frames_1(i,:), flipped_frame);
+endfor
+zero_idx = length(m_frames_1(1,:))+1;
+start_idx = zero_idx + cast(1.0/400 * fs_1, "int16");
+end_idx = zero_idx + cast(1.0/80 * fs_1, "int16");
+for i=1:length(acf_1(:,1))
+	[max_val, max_index] = max(acf_1(i, start_idx:end_idx));
+	true_max_idx = start_idx+max_index-1-zero_idx;
+	f_period_len = double(true_max_idx)/double(fs_1);
+	f_freqs_1(i) = 1/f_period_len;
+endfor
+x1= v_time_frame_1/fs_1;
+
+
 figure
 imagesc(v_time_1/fs_1, v_freq_1, 10*log10(max(abs(m_stft_1).^2, 10^(-15))))
 axis xy
+hold on
+plot(x1,f_freqs_1, 'r', 'LineWidth',2)
+hold off
+
 figure
 imagesc(v_time_2/fs_2, v_freq_2, 10*log10(max(abs(m_stft_2).^2, 10^(-15))))
 axis xy
